f2956a83f73982ff27af96a20dafab707bc1825c
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-10 16:42:34 -0300

    Criando show, delete, edit

--- app/controllers/book_currents_controller.rb
+++ app/controllers/book_currents_controller.rb
@@ -4,6 +4,40 @@ class BookCurrentsController < ApplicationController
   end
 
   def show
-    @book_currents = BookCurrent.find(params[:id])
+    @book_current = BookCurrent.find(params[:id])
+  end
+
+  def new
+    @book_current = BookCurrent.new
+  end
+
+  def create
+    @book_current = BookCurrent.new(book_current_params)
+
+    if @book_current.save
+      redirect_to @book_current
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book_current = BookCurrent.find(params[:id])
+  end
+
+  def update
+    @book_current = BookCurrent.find(params[:id])
+
+    if @book_current.update(book_current_params)
+      redirect_to @book_current
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_current_params
+    params.require(:book_current).permit(:current_type)
   end
 end
