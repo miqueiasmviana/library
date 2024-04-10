499dfd98fc32d475d790a9a29b2f03261213a819
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-10 14:12:47 -0300

    Criando metodos edit update, destroy. Criando _form.html.erb. E renderizando edit e show.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,27 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to book_path, status: :see_other
+  end
+
   private
   def book_params
     params.require(:book).permit(:cover, :title, :author, :description, :genre, :publish_company, :page, :year_publish)
