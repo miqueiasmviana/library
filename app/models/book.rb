29bc87b9d77c95d3d72ae9e669afae549d9ccfac
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-10 12:26:39 -0300

    Configurando new, create e book_params.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  validates :cover, presence: true
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :genre, presence: true
+  validates :publish_company, presence: true
+  validates :page, presence: true
+  validates :year_publish, presence: true
 end
+
