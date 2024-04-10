6a6ec41c93b677ea6be567197207658bab098a39
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-10 18:10:45 -0300

    Adicionando controller e view para welcome e configurando view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "books#index"
+  root "welcome#index"
 
+
+  get 'welcome/index'
   resources :book_currents
   resources :books
 
