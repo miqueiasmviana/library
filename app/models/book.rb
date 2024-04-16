34ec3f9df2c2d40d35f33f7174926be284f58549
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-16 18:06:08 -0300

    ajustando

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
-
   belongs_to :book_current
 
   validates :cover, :title, :author, :description, :genre, :publish_company, :page, :year_publish, :book_current_id, presence: true
